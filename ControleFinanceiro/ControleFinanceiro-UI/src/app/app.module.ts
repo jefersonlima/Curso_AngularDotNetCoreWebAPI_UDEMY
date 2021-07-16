b2a777a8170fd2d29574b9845d1a3c94d86ed7b0
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-07-16 06:22:30 -0400

    Aula 13. Criando o primeiro serviço no Angular

--- ControleFinanceiro/ControleFinanceiro-UI/src/app/app.module.ts
+++ ControleFinanceiro/ControleFinanceiro-UI/src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { TiposService } from '../app/services/tipos.service';
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    TiposService,
+    HttpClientModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
