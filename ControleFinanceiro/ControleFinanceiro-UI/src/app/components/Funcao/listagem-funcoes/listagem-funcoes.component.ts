f2f2316cd29d288e207fc2fb5c90a95309ba930d
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-07-27 13:09:45 -0400

    Aula 40. Criando o autocomplete de funções

--- ControleFinanceiro/ControleFinanceiro-UI/src/app/components/Funcao/listagem-funcoes/listagem-funcoes.component.ts
+++ ControleFinanceiro/ControleFinanceiro-UI/src/app/components/Funcao/listagem-funcoes/listagem-funcoes.component.ts
@@ -1,3 +1,6 @@
+import { startWith, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
 import { FuncoesService } from './../../../services/funcoes.service';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
@@ -13,6 +16,9 @@ export class ListagemFuncoesComponent implements OnInit {
 
   funcoes = new MatTableDataSource<any>()
   displayedColumns: string[] | undefined;
+  autoCompleteInput = new FormControl;
+  opcoesFuncoes: string[] = [];
+  nomesFuncoes: Observable<string[]> | undefined;
 
   @ViewChild(MatPaginator, { static: true })
   paginator: MatPaginator | undefined;
@@ -24,15 +30,35 @@ export class ListagemFuncoesComponent implements OnInit {
 
   ngOnInit(): void {
     this.funcoesService.PegarTodos().subscribe(resultado => {
+      resultado.forEach(funcao => {
+        this.opcoesFuncoes.push(funcao.name!);
+      });
       this.funcoes.data = resultado;
       this.funcoes.sort = this.sort!;
       this.funcoes.paginator = this.paginator!;
     });
     this.displayedColumns = this.ExibirColunas();
+
+    this.nomesFuncoes = this.autoCompleteInput.valueChanges.pipe(startWith(''), map(nome => this.FiltrarNomes(nome)))
   }
 
   ExibirColunas(): string[] {
     return ['nome', 'descricao', 'acoes'];
   }
 
+  FiltrarNomes(nome: string): string[] {
+    if (nome.trim().length >= 4) {
+      this.funcoesService.FiltrarFuncoes(nome.toLowerCase()).subscribe(resultado => {
+        this.funcoes.data = resultado;
+      });
+    } else {
+      if (nome === '') {
+        this.funcoesService.PegarTodos().subscribe((resultado) => {
+          this.funcoes.data = resultado;
+        });
+      }
+    }
+    return this.opcoesFuncoes.filter((funcao) =>
+      funcao.toLowerCase().includes(nome.toLowerCase()));
+  }
 }
